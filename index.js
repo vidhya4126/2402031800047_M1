98ed2fc63803737e94cdae50667f70bd8655a969
vidhya4126 <vidhya.popat01@gmail.com>
2026-05-26 13:28:41 +0530

    26-05-2026 updated

--- index.js
+++ index.js
@@ -6,5 +6,31 @@ app.listen(3001, () => {
 })
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World!</h1>")
+    res.send("<h1>Welcome to the Home Page</h1>")
+})
+
+app.get('/about', (req, res) => {
+    res.send("<h1>About Page</h1>")
+})
+
+app.get('/gallery', (req, res) => {
+    res.send("<h1>Gallery Page</h1>")
+})
+
+app.get('/about/user', (req, res) => {
+    res.send("<h1>User Page</h1>")
+})
+
+app.get('/random.text', (req, res) => {
+    res.send("<h1>Random Page</h1>")
+})
+
+app.get('/user/:userid-:bookid', (req, res) => {
+    res.send( req.params)
+})
+
+app.get('/search', (req, res) => {
+    const name= req.query.name
+    const age= req.query.age
+    res.send(`Search results for Name: ${name}, Age: ${age}`)
 })
