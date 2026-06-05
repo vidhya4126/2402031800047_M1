d7c72c8aaabf7943218ea5eef9b09ee95af098c8
vidhya4126 <vidhya.popat01@gmail.com>
2026-06-05 11:39:42 +0530

    05-06-2026 EJS-PROJECT nodejs

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 
-const mongoose = require('mongoose')
-const Contact = require('./models/contacts.models')
-
-//Database connection
-mongoose.connect( 'mongodb://127.0.0.1:27017/contact-crud')
-.then(() => console.log('Database connected successfully'))
-
-//Middleware
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
+app.set('views', './views')
 
-//routes
-app.get('/', async (req, res) => {
-  const contacts = await Contact.find()
-  res.render('home',{ contacts }) 
-  })
+app.get('/', (req, res) => {
+    res.send("<h1>Home page</h1>")
+})
 
-app.get('/show-contact', (req, res) => { res.render('show-contact') })
+app.get('/about', (req, res) => {
+    var users = [
+        {name: 'John', age: 30},
+        {name: 'Jane', age: 25},
+        {name: 'Bob', age: 35}
+    ];
 
-app.get('/add-contact', (req, res) => { res.render('add-contact') })
+    res.render('about', { 
+                            title: 'About Page',
+                            message: 'Welcome',
+                            items: users
+                    })
+})
 
-app.post('/add-contact', (req, res) => {})
+app.get('/form', (req, res) => {
+    res.render('form')
+})
 
-app.get('/update-contact', (req, res) => { res.render('update-contact') })
-
-app.post('/update-contact', (req, res) => {})
-
-app.get('/delete-contact', (req, res) => {})
- 
-
-app.listen(3002, () => {
-  console.log('Server started successfully on port 3000')
+app.listen(3001, () => {
+    console.log('Server is running on port 3001')
 })
